1a2449e2f7df6e1e94533109507329c7fa5c5f3d
neil homan <neilhoman@gmail.com>
2018-04-30 22:16:46 +0100

    removed testing code from package main

--- cmd/chat/utils.go
+++ cmd/chat/utils.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
-	"path/filepath"
-	"reflect"
-	"runtime"
-	"testing"
 )
 
 //StringInSlice looksup a string in a slice returns true if found
@@ -34,33 +29,4 @@ func GetConfigFromJSON(filename string, configuration interface{}) (err error) {
 		return err
 	}
 	return err
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////
-
-// Assert fails the test if the condition is false.
-func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
-	if !condition {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("%s:%d: "+msg+"\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
-		tb.FailNow()
-	}
-}
-
-// Ok fails the test if an err is not nil.
-func Ok(tb testing.TB, err error) {
-	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("%s:%d: unexpected error: %s\n\n", filepath.Base(file), line, err.Error())
-		tb.FailNow()
-	}
-}
-
-// Equals fails the test if exp is not equal to act.
-func Equals(tb testing.TB, exp, act interface{}) {
-	if !reflect.DeepEqual(exp, act) {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\n\n", filepath.Base(file), line, exp, act)
-		tb.FailNow()
-	}
 }
