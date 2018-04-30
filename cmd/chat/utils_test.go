d64705aea8d87d57918f53790ff4c10a80516035
neil homan <neilhoman@gmail.com>
2018-04-30 22:18:37 +0100

    moved test functions to test file

--- cmd/chat/utils_test.go
+++ cmd/chat/utils_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
@@ -19,5 +23,34 @@ func TestStringInSlice(t *testing.T) {
 		got, err := StringInSlice(c.s, c.search)
 		Ok(t, err)
 		Equals(t, c.want, got)
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Assert fails the test if the condition is false.
+func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d: "+msg+"\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
+
+// Ok fails the test if an err is not nil.
+func Ok(tb testing.TB, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d: unexpected error: %s\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+// Equals fails the test if exp is not equal to act.
+func Equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
 	}
 }
